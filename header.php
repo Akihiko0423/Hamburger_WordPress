9bdbaaf38e0d783b195b9c552506208f0a83c268
Akihiko <yama19980423dada@gmail.com>
2021-09-27 19:35:37 +0900

    09/27

--- header.php
+++ header.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
-<html lang="ja">
+<html <?php language_attributes(); ?>>
   <head>
-    <meta charset="UTF-8">
+    <meta charset="<?php bloginfo('charset'); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="shortcut icon" href="<?php echo get_template_directory_uri(); ?>/assets/img/hamburger.jpg">
     <?php wp_head(); ?>
