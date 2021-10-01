19a42507501299165fa240d4f49162e34bbcf17c
Akihiko <yama19980423dada@gmail.com>
2021-10-01 22:34:42 +0900

    10/01

--- single.php
+++ single.php
@@ -2,165 +2,26 @@
   <!--main part-->
   <main class = "l-main">
     <!--mainvisual-->
-    <div class = "p-single-main-visual">
-      <?php the_post_thumbnail('large'); ?>
-      <h1><?php the_title(); ?></h1>
-    </div>
-    <div class = "p-single-main-heading">
-      <h2>見出しh2</h2>
-      <p class = "p-heading-text">
-        pタグテキスト。pタグテキスト。pタグテキスト。pタグテキスト。
-        pタグテキスト。pタグテキスト。pタグテキスト。pタグテキスト。
-        pタグテキスト。pタグテキスト。pタグテキスト。pタグテキスト。
-        pタグテキスト。pタグテキスト。pタグテキスト。pタグテキスト。
-        pタグテキスト。pタグテキスト。pタグテキスト。pタグテキスト。
-        pタグテキスト。pタグテキスト。pタグテキスト。pタグテキスト。
-      </p>
-      <h3>見出しh3</h3>
-      <h4>見出しh4</h4>
-      <h5>見出しh5</h5>
-      <h6>見出しh6</h6>
-      <div class = "p-cite-tag">
-        <!--引用タグ-->
-        <blockquote>
-          <p class = "p-cite-tag-quote">
-            Blockquote引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ
-            引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ
-            引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ
-            引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ
-            引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ
-            引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ
-            引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ
-            引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ
-            引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ
-            引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ
-          </p>
-          <p class = "p-cite-tag-source">出典元: <a href="#">〇〇〇〇〇〇〇〇〇〇〇〇</a></p>
-        </blockquote>
+    <div class = "p-single">
+      <h2 class = "p-single__title"><?php the_title(); ?></h2>
+      <div class = "p-single__inner">
+        <div class = "p-single__inner__visual">
+          <?php 
+          $pic = get_field('pic'); 
+          $pic_url = $pic['sizes']['large'];
+          ?>
+          <img src="<?php echo $pic_url; ?>" alt="画像">
+        </div>
+        <div class = "p-single__inner__text">
+          <p>価格:<?php the_field('price'); ?>円(税込み)</p>
+          <p>カロリー:<?php echo number_format(get_field('calorie')); ?>kcal</p>
+        </div>
+      </div>
+      <div class = "content">
+        <?php the_content(); ?>
       </div>
     </div>
-    <!--たくさんの写真の部分-->
-    <div class = "p-single-main-many-photo">
-      <div class = "p-photo-big"></div>
-      <div class = "p-photo-left">
-        <div class = "c-photo-left"></div>
-        <p>
-          テキストが入ります　テキストが入ります　テキストが入ります　テキストが入ります
-          テキストが入ります　テキストが入ります　テキストが入ります　テキストが入ります
-          <span class = "p-pc-text">
-            テキストが入ります　テキストが入ります　テキストが入ります　テキストが入ります
-            テキストが入ります　テキストが入ります　テキストが入ります　テキストが入ります
-            テキストが入ります　テキストが入ります　テキストが入ります　テキストが入ります
-            テキストが入ります　テキストが入ります　テキストが入ります　テキストが入ります
-            テキストが入ります　テキストが入ります　テキストが入ります　テキストが入ります
-            テキストが入ります　テキストが入ります　テキストが入ります　テキストが入ります
-            テキストが入ります　テキストが入ります　テキストが入ります　テキストが入ります
-            テキストが入ります　テキストが入ります　
-          </span>
-        </p>
-      </div>
-      <div class = "p-photo-right">
-        <p>
-          テキストが入ります　テキストが入ります　テキストが入ります　テキストが入ります
-          テキストが入ります　テキストが入ります　テキストが入ります　テキストが入ります
-          <span class = "p-pc-text">
-            テキストが入ります　テキストが入ります　テキストが入ります　テキストが入ります
-            テキストが入ります　テキストが入ります　テキストが入ります　テキストが入ります
-            テキストが入ります　テキストが入ります　テキストが入ります　テキストが入ります
-            テキストが入ります　テキストが入ります　テキストが入ります　テキストが入ります
-            テキストが入ります　テキストが入ります　テキストが入ります　テキストが入ります
-            テキストが入ります　テキストが入ります　テキストが入ります　テキストが入ります
-            テキストが入ります　テキストが入ります　テキストが入ります　テキストが入ります
-            テキストが入ります　テキストが入ります　
-          </span>
-        </p>
-        <div class = "c-photo-right"></div>
-      </div>
-      <div class = "p-photo-center"></div>
-      <div>
-        <ul class = "p-photo-many">
-          <li class = "p-photo-many-flex"></li>
-          <li class = "p-photo-many-flex"></li>
-          <li class = "p-photo-many-flex"></li>
-          <li class = "p-photo-many-flex"></li>
-          <li class = "p-photo-many-flex"></li>
-          <li class = "p-photo-many-flex"></li>
-          <li class = "p-photo-many-flex"></li>
-          <li class = "p-photo-many-flex"></li>
-          <li class = "p-photo-many-flex"></li>
-        </ul>
-      </div>
-    </div>
-    <!--list part-->
-    <div class = "p-single-main-list"> 
-      <ul class = "c-list">
-        <li class = "c-list-first">
-          1.リストリストリスト<br>
-          1.リストリストリスト
-        </li>
-        <li class = "c-list-second">
-          1.リスト2リスト2リスト2<br>
-          1.リスト2リスト2リスト2
-        </li>
-        <li class = "c-list-third">
-          1.リストリストリスト<br>
-          1.リストリストリスト
-        </li>
-      </ul>
-      <ul class = "c-list">
-        <li class = "c-list-first">
-          リストリストリスト<br>
-          リストリストリスト
-        </li>
-        <li class = "c-list-second">
-          リスト2リスト2リスト2<br>
-          リスト2リスト2リスト2
-        </li>
-        <li class = "c-list-third">
-          リストリストリスト<br>
-          リストリストリスト
-        </li>
-      </ul>
-    </div>
-    <div class = "p-single-main-html-text">
-      <div class = "c-html-text">
-        <!--特殊演算子利用 url:https://proengineer.internous.co.jp/content/columnfeature/5124-->
-          &lt;html&gt;<br>
-          &emsp;&emsp;&ensp;&lt;head&gt;<br>
-          &emsp;&emsp;&ensp;&lt;/head&gt;<br>
-          &emsp;&emsp;&ensp;&lt;body&gt;<br>
-          &emsp;&emsp;&ensp;&lt;/body&gt;<br>
-          &lt;html&gt;<br>
-        </p>
-      </div>
-    </div>
-    <!--table part-->
-    <div class = "p-single-main-table">
-      <table>
-        <tr>
-          <td class = "c-first-column">テーブル</td>
-          <td class = "c-second-column">テーブル</td>
-        </tr>
-        <tr>
-          <td>テーブル</td>
-          <td>テーブル</td>
-        </tr>
-        <tr>
-          <td>テーブル</td>
-          <td>テーブル</td>
-        </tr>
-        <tr>
-          <td>テーブル</td>
-          <td>テーブル</td>
-        </tr>
-      </table>
-    </div>
-    <div class = "c-single-main-btn">
-      <a href="#">ボタン</a>
-    </div>
-    <div class = "c-single-main-bold">
-      <p>boldboldboldboldboldboldbold</p>
-    </div>
+
   </main>
   <!--sidebar part-->
   <?php get_sidebar(); ?>
